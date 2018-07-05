caaf09f6436a19d15e9bf5c4f135751cb891cb39
Francesco Gazzetta <francygazz@gmail.com>
2018-07-05 09:07:11 +0200

    Moving some things from private overlay
    ...mostly games :D

--- default.nix
+++ default.nix
@@ -1,4 +1,15 @@
-{ callPackage }:
+{ callPackage, libsForQt5 }:
 {
+  ### DEVELOPMENT
+  lmdbxx = callPackage ./pkgs/lmdbxx { };
+
+  ### APPLICATIONS
+  variety = callPackage ./pkgs/variety { };
+
+  ### GAMES
+  _20kly = callPackage ./pkgs/20kly { };
+  endgame-singularity = callPackage ./pkgs/endgame-singularity { };
+  openhexagon = callPackage ./pkgs/openhexagon { };
+  powermanga = libsForQt5.callPackage ./pkgs/powermanga { };
 }
 
