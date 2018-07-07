5e6fc20a4904dad009832361f433d6f30602d4e9
Francesco Gazzetta <francygazz@gmail.com>
2018-07-07 16:40:10 +0200

    Fix overlay

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,5 @@
 self: super:
 
 # Should work for basic usage
-import ./default.nix { callPackage = super.callPackage; }
+import ./default.nix { callPackage = super.callPackage; libsForQt5 = super.libsForQt5; }
 
