81077638f754352d530ac884fed6c6fca6f01fa0
Francesco Gazzetta <francygazz@gmail.com>
2018-08-22 19:53:12 +0200

    Fix sfxr install

--- pkgs/sfxr/default.nix
+++ pkgs/sfxr/default.nix
@@ -1,8 +1,9 @@
 { stdenv, fetchurl
-, pkgconfig
+, pkgconfig, desktop-file-utils
 , SDL, gtk3
 }:
 
+# Builds but can't find files at runtime because of hardcoded paths
 stdenv.mkDerivation rec {
   name = "sfxr-${version}";
   version = "1.2.1";
@@ -10,7 +11,11 @@ stdenv.mkDerivation rec {
     url = "http://www.drpetter.se/files/sfxr-sdl-${version}.tar.gz";
     sha256 = "0dfqgid6wzzyyhc0ha94prxax59wx79hqr25r6if6by9cj4vx4ya";
   };
-  nativeBuildInputs = [ pkgconfig ];
+  postPatch = ''
+    substituteInPlace Makefile --replace "usr/" ""
+  '';
+  nativeBuildInputs = [ pkgconfig desktop-file-utils ];
   buildInputs = [ SDL gtk3 ];
+  makeFlags = [ "DESTDIR=$(out)" ];
 }
 
